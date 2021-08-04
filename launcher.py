e0926f217589398c7a2b0efdec330bd484629034
Carlos Cagigao Bravo <carlos.cagigaobravo@gmail.com>
2021-08-04 17:59:20 +0200

    feat: add log and basic file read

--- launcher.py
+++ launcher.py
@@ -1,6 +1,7 @@
 import requests, time, sys, signal
 import argparse
 import urllib.request
+from pwn import log
 
 # Constant variables
 DESCRIPTION = "Script that launches some URIs to specific URL"
@@ -21,6 +22,11 @@ FILE_HELP = "File that contains some URIs to launch. This file must be formatted
     By default: '%s'" % FILE_DEFAULT
 FILE_VARIABLE_NAME = "file_location"
 
+LOG_INFO_MAIN_INIT = "Sending attacks to %s"
+LOG_PROGRESS_FILE = "File line number"
+LOG_PROGRESS_URL = "Launching URL"
+LOG_INFO_MAIN_END = "File successfully parsed. Number of URL launched: %s"
+
 # Init parser parameters
 parser = argparse.ArgumentParser(description=DESCRIPTION)
 parser.add_argument(URL_ARGUMENT, help=URL_HELP, default=URL_DEFAULT, metavar=URL_VARIABLE_NAME, \
@@ -31,8 +37,19 @@ parser.add_argument(FILE_ARGUMENT, help=FILE_HELP, default=FILE_DEFAULT, metavar
     dest=FILE_VARIABLE_NAME)
 args = parser.parse_args()
 
-# Sending help if no arguments provided
-if (len(sys.argv) < 2):
-    parser.print_help(sys.stderr)
-
-print(args)
+# Main 
+log.info(LOG_INFO_MAIN_INIT % args.url)
+progress_file = log.progress(LOG_PROGRESS_FILE)
+progress_url_launch = log.progress(LOG_PROGRESS_URL)
+try:
+    with open(args.file_location, 'r') as file:
+        count = 1
+        for line in file:
+            progress_file.status("%s" % count)
+            progress_url_launch.status("%s" % line)
+            count += 1
+            #time.sleep(1)
+    file.close()
+    log.info(LOG_INFO_MAIN_END % count)
+except FileNotFoundError:
+    log.error("File %s does not exist" % args.file_location)
